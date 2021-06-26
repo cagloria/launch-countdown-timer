1d149b9962743715eb87bc3958728d056f1f774a
cagloria <33134245+cagloria@users.noreply.github.com>
2021-06-25 17:11:38 -0700

    Added base content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import facebook from "./assets/icon-facebook.svg";
+import pinterest from "./assets/icon-pinterest.svg";
+import instagram from "./assets/icon-instagram.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <main>
+                <h1>We're launching soon</h1>
+
+                <div>
+                    <div>
+                        <div>08</div>
+                        <p>Days</p>
+                    </div>
+                    <div>
+                        <div>23</div>
+                        <p>Hours</p>
+                    </div>
+                    <div>
+                        <div>55</div>
+                        <p>Minutes</p>
+                    </div>
+                    <div>
+                        <div>41</div>
+                        <p>Seconds</p>
+                    </div>
+                </div>
+            </main>
+            <footer>
+                <a href="#">
+                    <img src={facebook} alt="Facebook" />
+                </a>
+                <a href="#">
+                    <img src={pinterest} alt="Pinterest" />
+                </a>
+                <a href="#">
+                    <img src={instagram} alt="Instagram" />
+                </a>
+                <p>
+                    Challenge by{" "}
+                    <a
+                        href="https://www.frontendmentor.io?ref=challenge"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Frontend Mentor
+                    </a>
+                    . Coded by{" "}
+                    <a href="https://github.com/cagloria">C.A. Gloria</a>.
+                </p>
+            </footer>
+        </>
+    );
 }
 
 export default App;
